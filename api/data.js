9ce2dbc2e3abf4bfa513d5c6f0f83c79a7f8df16
Anand Subramanian <anand.founder@gmail.com>
2025-09-16 13:52:40 -0500

    July and Aug 2025 Data

--- api/data.js
+++ api/data.js
@@ -1,5 +1,7 @@
 export default function handler(req, res) {
   const layoffsData = [
+    { month: "Aug", year: 2025, number: 85979, sector: ["Multiple"] },
+    { month: "Jul", year: 2025, number: 62075, sector: ["Multiple"] },
     { month: "Jun", year: 2025, number: 10000, sector: ["Multiple"] },
     { month: "May", year: 2025, number: 10397, sector: ["Multiple"] },
     { month: "Apr", year: 2025, number: 24500, sector: ["Multiple"] },
@@ -9,6 +11,8 @@ export default function handler(req, res) {
   ];
 
   const opportunitiesData = [
+    { month: "Aug", year: 2025, number: 22000, sector: ["Multiple"] },
+    { month: "Jul", year: 2025, number: 79000, sector: ["Multiple"] },
     { month: "Jun", year: 2025, number: 21500, sector: ["Multiple"] },
     { month: "May", year: 2025, number: 20000, sector: ["Multiple"] },
     { month: "Apr", year: 2025, number: 18500, sector: ["Multiple"] },
